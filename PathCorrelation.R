3eab9f9a46a5777be1613d749ac4d72bf7a49850
CIG-spatialLandscape <121252795+CIG-spatialLandscape@users.noreply.github.com>
2023-03-04 12:38:29 +0100

    Update PathCorrelation.R

--- PathCorrelation.R
+++ PathCorrelation.R
@@ -2,7 +2,7 @@
 ## Project: Cancer Hallmarks
 ## Script purpose: Correlate hallmark activity and pathway scores across all samples
 ## Date: 22/12/2022
-## Author: Sergi Cervilla & Mustafa Sibai
+## Author: Sergi Cervilla * & Mustafa Sibai *
 ##################################################
 
 library(dplyr)

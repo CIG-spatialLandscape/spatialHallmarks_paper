a430906c7f4df37ae2484d4e30eae78ae6ef12dd
CIG-spatialLandscape <121252795+CIG-spatialLandscape@users.noreply.github.com>
2023-03-04 12:33:25 +0100

    Update GeneCollection.R

--- GeneCollection.R
+++ GeneCollection.R
@@ -2,7 +2,7 @@
 ## Project: Cancer Hallmarks
   ## Script purpose: Obtain hallmark signatures using Pathway Commons
 ## Date: 22/12/2022
-## Author: Sergi Cervilla & Mustafa Sibai
+## Author: Sergi Cervilla * & Mustafa Sibai *
 ##################################################
 
 
